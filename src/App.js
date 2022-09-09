0bc91f6c9e4b49a1fd5b70a4a1f9abeffff68c6d
Rohit Bahuguna <rohitbahuguna.4446@gmail.com>
2022-09-09 23:18:30 +0530

    ficed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import PrivateComponent from './components/PrivateComponent';
 import Login from './components/Login';
@@ -10,26 +10,25 @@ import ProductList from './components/ProductList';
 import UpdateProduct from './components/UpdateComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter >
-      <Nav />
-      <Routes>
-        <Route element={<PrivateComponent />}>
-        <Route path='/' element={<ProductList />} />
-        <Route path='/add' element={<AddProduct />} />
-        <Route path='/update/:id' element={<UpdateProduct />} />
-        <Route path='/logout' element={<h1>Logout Component</h1>} />
-        <Route path='/profile' element={<h1>Profile Component</h1>} />s
-        
-        </Route>
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-      </BrowserRouter>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Nav />
+				<Routes>
+					<Route element={<PrivateComponent />}>
+						<Route path="/" element={<SignUp />} />
+						<Route path="/add" element={<AddProduct />} />
+						<Route path="/update/:id" element={<UpdateProduct />} />
+						<Route path="/logout" element={<h1>Logout Component</h1>} />
+						<Route path="/profile" element={<h1>Profile Component</h1>} />s
+					</Route>
+					<Route path="/signup" element={<SignUp />} />
+					<Route path="/login" element={<Login />} />
+				</Routes>
+			</BrowserRouter>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
